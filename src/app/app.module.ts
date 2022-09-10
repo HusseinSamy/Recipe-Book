0ff9d916aa7fa87fd8cd499cb6d362fb96aef489
Hussein Samy <hussein.samy02@gmail.com>
2022-09-10 20:31:02 +0200

    Formats code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http'
-
-import { HeaderComponent } from './header/header.component'
 import { AppRoutingModule } from './routing.module';
-import { ShoppingListModule } from './shopping-list.module';
-import { RecipesModule } from './recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { RecipesModule } from './recipes/recipes.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
