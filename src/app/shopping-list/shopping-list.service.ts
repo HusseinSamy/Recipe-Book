7d9235888e872844fadaee4f7b9c0ea66fe1de40
Hussein Samy <hussein.samy02@gmail.com>
2022-09-11 02:09:30 +0200

    Adds rxjs Subjects and and getters functions

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/Models/ingredient.model";
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class ShoppingListService{
 
-
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples',5)
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
   ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
 
 
   addItem(data: Ingredient)
   {
     this.ingredients.push(data)
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   deleteItem()
